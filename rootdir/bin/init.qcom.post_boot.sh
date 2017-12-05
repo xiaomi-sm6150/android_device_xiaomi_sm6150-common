3135fd7008585ce09b7077f97eb8b22fb5abc2df
Robb Glasser <rglasser@google.com>
2017-12-04 17:28:08 -0800

    sm6150-common: rootdir: Enable idle_state mechanism
    Allows us to check the idle state of the display in order to use perf
    hints to improve system performance.
    
    Bug: 69270928
    Bug: 69915886
    
    Test: Booted and cat-ed the affected files
    Change-Id: I8717093e9b8ed9ed35b6ca51a20225abbf71f775

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -182,6 +182,10 @@ case "$soc_id" in
     echo 2-5     > /dev/cpuset/system-background/cpus
     echo 2-5     > /dev/cpuset/restricted/cpus
 
+    # Enable idle state listener
+    echo 1 > /sys/class/drm/card0/device/idle_encoder_mask
+    echo 100 > /sys/class/drm/card0/device/idle_timeout_ms
+
     # Turn on sleep modes.
     echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
     ;;
@@ -295,6 +299,10 @@ case "$soc_id" in
     echo 4-5     > /dev/cpuset/background/cpus
     echo 2-5     > /dev/cpuset/system-background/cpus
     echo 2-5     > /dev/cpuset/restricted/cpus
+
+    # Enable idle state listener
+    echo 1 > /sys/class/drm/card0/device/idle_encoder_mask
+    echo 100 > /sys/class/drm/card0/device/idle_timeout_ms
 
     # Turn on sleep modes.
     echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
