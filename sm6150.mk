9010fc737d147ed87058471766cd33ccfbd5a687
Jenhao Chen <jenhaochen@google.com>
2019-03-20 14:57:51 -0700

    sm6150-common: Enable zram writeback
    Bug: 128889899
    Test: Boot to home
    
    Change-Id: I48ae06e54a66a7d6965afbe8db3d2f157cf8a1f4

--- sm6150.mk
+++ sm6150.mk
@@ -380,3 +380,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
+
+# ZRAM writeback
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.zram.mark_idle_delay_mins=60 \
+    ro.zram.first_wb_delay_mins=180 \
+    ro.zram.periodic_wb_delay_hours=24
