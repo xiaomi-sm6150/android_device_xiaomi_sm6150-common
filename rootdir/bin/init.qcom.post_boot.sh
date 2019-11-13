c0816640a1713b1e009aff80ba7b6a44e538d781
Kyle Lin <kylelin@google.com>
2019-11-13 11:41:12 +0800

    sm6150-common: rootdir: Modify the cpuset setting
    Because the arch is similiar with SDM670, we aling the setting with
    bonito.
    
    Bug: 142552311
    Test: build , munaual check the settings
    Change-Id: Ica44125c8ed71a85cfd6bde77c9107bceed7b094

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -193,8 +193,11 @@ case "$soc_id" in
     done
 
     # cpuset parameters
-    echo 0-5 > /dev/cpuset/background/cpus
-    echo 0-5 > /dev/cpuset/system-background/cpus
+    echo 0-7     > /dev/cpuset/top-app/cpus
+    echo 0-5,7 > /dev/cpuset/foreground/cpus
+    echo 4-5     > /dev/cpuset/background/cpus
+    echo 2-5     > /dev/cpuset/system-background/cpus
+    echo 2-5     > /dev/cpuset/restricted/cpus
 
     # Turn off scheduler boost at the end
     echo 0 > /proc/sys/kernel/sched_boost
@@ -325,10 +328,10 @@ case "$soc_id" in
 
     # cpuset parameters
     echo 0-7     > /dev/cpuset/top-app/cpus
-    echo 0-2,4-7 > /dev/cpuset/foreground/cpus
-    echo 0-2     > /dev/cpuset/background/cpus
-    echo 0-3     > /dev/cpuset/system-background/cpus
-    echo 0-3     > /dev/cpuset/restricted/cpus
+    echo 0-5,7 > /dev/cpuset/foreground/cpus
+    echo 4-5     > /dev/cpuset/background/cpus
+    echo 2-5     > /dev/cpuset/system-background/cpus
+    echo 2-5     > /dev/cpuset/restricted/cpus
 
     # Turn off scheduler boost at the end
     echo 0 > /proc/sys/kernel/sched_boost
