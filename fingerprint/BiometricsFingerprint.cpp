5237fef4f984091cb9d30f2d5765771b03e3556c
Pig <pig.priv@gmail.com>
2020-05-18 19:04:57 +0200

    sm6150-common: fingerprint: Add Xiaomi fingerprintextension support
    * Import fingerprint.h from https://android.googlesource.com/platform/hardware/libhardware/+/d88dfe8607af019186a309674d9081a056ed3daa
      and add the xiaomi fingerprint extension command.
    
    Co-authored-by: C457 <android.c357@gmail.com>
    Change-Id: Ie9deb0dfcc689034cd3c4b9095e319130ba8d8cd

--- fingerprint/BiometricsFingerprint.cpp
+++ fingerprint/BiometricsFingerprint.cpp
@@ -20,7 +20,6 @@
 #include <hardware/hw_auth_token.h>
 
 #include <hardware/hardware.h>
-#include <hardware/fingerprint.h>
 #include "BiometricsFingerprint.h"
 
 #include <cutils/properties.h>
@@ -379,6 +378,10 @@ void BiometricsFingerprint::notify(const fingerprint_msg_t *msg) {
             }
             break;
     }
+}
+
+Return<int32_t> BiometricsFingerprint::extCmd(int32_t cmd, int32_t param) {
+    return mDevice->extCmd(mDevice, cmd, param);
 }
 
 } // namespace implementation
