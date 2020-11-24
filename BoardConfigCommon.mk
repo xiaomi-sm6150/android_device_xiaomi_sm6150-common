7f66c4e8f4445f271761100246cf8cfa512473fe
raghavt20 <raghavtandon2000@gmail.com>
2020-11-24 12:59:08 +0530

    sm6150-common: Drop sepolicy includes

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -146,7 +146,6 @@ VENDOR_SECURITY_PATCH := 2020-11-01
 
 # Sepolicy
 TARGET_SEPOLICY_DIR := msmsteppe
-include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
