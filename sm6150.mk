e7ffc534b1baaa430d030b393fdd7a132941d7f4
raghavt20 <raghavtandon2000@gmail.com>
2020-11-24 12:57:14 +0530

    sm6150-common: Inherit common QTI-Perf component

--- sm6150.mk
+++ sm6150.mk
@@ -261,6 +261,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
     $(LOCAL_PATH)/configs/permissions/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
 
+# QTI Vendor Components
+TARGET_COMMON_QTI_COMPONENTS := \
+    perf
+
 # RIL
 PRODUCT_PACKAGES += \
     libjson \
