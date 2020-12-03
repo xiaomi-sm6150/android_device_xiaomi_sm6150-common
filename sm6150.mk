30fb3a06818ccfc87795a61b3e36704560d224c9
Alexander Koskovich <zvnexus@outlook.com>
2020-12-03 10:26:15 +0000

    sm6150-common: Disable the usage of ConfigStore
    - As per commit https://github.com/LineageOS/android_hardware_interfaces/commit/11a113a67e87a8da9a9bf0a73883f2325de90934 ("Add disable_configstore").
    
    Change-Id: I5482989a8a96227a3e2a5317eacf613c641bfa8e
    Signed-off-by: raghavt20 <raghavt20@aospa.co>

--- sm6150.mk
+++ sm6150.mk
@@ -69,6 +69,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# Configstore
+PRODUCT_PACKAGES += \
+    disable_configstore
+
 # Device-specific settings
 PRODUCT_PACKAGES += \
     XiaomiParts
