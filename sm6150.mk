5f989dc823b87aa6d8734e97d98634f170d8f06e
Arian <arian.kulmer@web.de>
2021-01-19 16:23:54 +0100

    sm6150-common: Remove lineage specific overlays
    Change-Id: If6698829575d6a4eba3d9f034f62f9a1ca43a6b8

--- sm6150.mk
+++ sm6150.mk
@@ -235,8 +235,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
