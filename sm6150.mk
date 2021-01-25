e76fb5010d886d840113e8cf8414b5af28481f50
Arian <arian.kulmer@web.de>
2021-01-25 12:42:07 +0100

    sm6150-common: Rename fingerprint service
    Change-Id: I09992ec66ba4b65bb60a23faa74929d385f199fa

--- sm6150.mk
+++ sm6150.mk
@@ -138,6 +138,9 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.3-service.clearkey
 
 # Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sm6150
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
