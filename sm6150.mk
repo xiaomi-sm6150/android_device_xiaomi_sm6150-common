e5aa0f8d22910ac7678aa9f78722eaddfe078105
Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
2021-04-26 08:56:46 +0530

    sm6150-common: Decommonize livedisplay stuff
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>

--- sm6150.mk
+++ sm6150.mk
@@ -198,10 +198,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.1-service.xiaomi_sm6150
-
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail.vendor \
