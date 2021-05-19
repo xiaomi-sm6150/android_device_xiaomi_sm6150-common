a5b2e953c50a405c48e3ee9faf8e53cd1de554fd
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-05-19 14:36:49 +0530

    sm6150-common: Update in-house QCOM WFD packages
    Signed-off-by: Rasenkai <rasenkai99@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -241,10 +241,10 @@ PRODUCT_COPY_FILES += \
 
 # WiFi Display
 PRODUCT_PACKAGES += \
-    libavservices_minijail_32 \
-    libdisplayconfig.qti \
+    libavservices_minijail \
+    libavservices_minijail.vendor \
     libnl \
-    libqdMetaData \
+    libwfdaac \
     libwfdaac_vendor
 
 PRODUCT_BOOT_JARS += \
