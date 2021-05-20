690cccbaba4802e8c8ea7c6e71be3ea941a424f7
Arian <arian.kulmer@web.de>
2021-05-20 20:06:39 +0200

    sm6150-common: Decommonize lights HAL and overlays
    * toco and sweet don't have a notification light and since backlight
      control has been moved to hwc this HAL is not needed for these targets.
    
    * Keep the HAL itself in common tree so that devices which need are able
      to build the lights HAL which is common for all targets with a
      notification light.
    
    Change-Id: Ia4c3c96e49b5ce58f4f55b92a65321fb258f8975

--- sm6150.mk
+++ sm6150.mk
@@ -194,10 +194,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/security/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
-# Lights
-PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.xiaomi_sm6150
-
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.1-service.xiaomi_sm6150
