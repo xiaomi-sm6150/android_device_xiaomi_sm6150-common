0575303e6de8671da5e71b78268dc89e241bf91d
Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
2021-05-22 16:26:04 +0530

    sm6150-common: Decommonize AOSP volume table copy
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>

--- sm6150.mk
+++ sm6150.mk
@@ -66,7 +66,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
 
 PRODUCT_COPY_FILES += \
-    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
