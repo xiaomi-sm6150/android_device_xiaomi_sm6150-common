ce60eb66d0c36717b82f933096ed489973f30e1c
Arian <arian.kulmer@web.de>
2021-05-31 18:26:18 +0200

    Revert "sm6150-common: Exclude CarrierConfig overlays from RRO"
    This reverts commit 959ceb5f99a0d194c575f9ef82e08d36764ee129.
    
    Change-Id: I0b82719f123218d0f63f621dfe588421217975ef

--- sm6150.mk
+++ sm6150.mk
@@ -239,9 +239,6 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
-
 # Perf
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.2.vendor
