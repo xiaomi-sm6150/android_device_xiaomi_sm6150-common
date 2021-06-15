255fba49708ac8f64a71339f4a9b7002f8e4a95c
Rasenkai <rasenkai99@gmail.com>
2021-06-15 22:47:32 +0530

    sm6150-common: Update BT packages
    Signed-off-by: Rasenkai <rasenkai99@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -81,7 +81,13 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    audio.bluetooth.default \
+    libbluetooth_audio_session
+
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.audio@2.0-impl \
+    com.qualcomm.qti.bluetooth_audio@1.0.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
