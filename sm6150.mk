d6504e59b6970811e189aff00dc19eb86dbf2301
raghavt20 <raghavtandon2000@gmail.com>
2021-06-30 04:01:20 +0530

    sm6150-common: Dont build OSS perf@2.2
    - It's already included in common QTI perf component
    
    Change-Id: I29668c1ce11378823cf43686697514bb1bef647d

--- sm6150.mk
+++ sm6150.mk
@@ -202,9 +202,6 @@ PRODUCT_PACKAGES += \
     ParanoidDoze
 
 # Perf
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.perf@2.2.vendor
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
     $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
