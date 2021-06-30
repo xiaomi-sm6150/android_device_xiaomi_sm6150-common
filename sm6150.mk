343cedf8bb681d2d384ed2fb6b6230f8a65d2bf0
raghavt20 <raghavtandon2000@gmail.com>
2021-06-30 14:54:43 +0530

    sm6150-common: Use common QTI usb component
    Change-Id: I25fc6121c3ee3e450611377b882a7af913127951

--- sm6150.mk
+++ sm6150.mk
@@ -216,6 +216,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
     gps \
     perf \
     telephony \
+    usb \
     wlan
 
 PRODUCT_COPY_FILES += \
@@ -258,14 +259,6 @@ PRODUCT_COPY_FILES += \
 # Uevent
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc
-
-# USB
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
 # Vibrator
 PRODUCT_PACKAGES += \
