57d5f1f1d413b6cb7eedf7cf5d96e20af440640c
Arvind Kumar <karvin@codeaurora.org>
2021-09-15 15:42:25 +0530

    sm6150-common: Set soc properties
    Set soc model and manufacturer properties
    
    Change-Id: Ieee34b7bfab87b241abc5ed3318b46a5c6f93456

--- sm6150.mk
+++ sm6150.mk
@@ -208,7 +208,8 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/bin/init.qcom.post_boot.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.post_boot.sh \
-    $(LOCAL_PATH)/rootdir/bin/init.qcom.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.sh
+    $(LOCAL_PATH)/rootdir/bin/init.qcom.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.sh \
+    $(LOCAL_PATH)/rootdir/bin/init.qti.early_init.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qti.early_init.sh
 
 # IRSC
 PRODUCT_COPY_FILES += \
