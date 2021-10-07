02c8a1e934d6ae727566bfa6a9a3cf0bf3a6054e
Arian <arian.kulmer@web.de>
2021-10-07 13:36:03 +0200

    sm6150-common: Disable recovery updater lib for now
    Change-Id: Ibd5dd8f8b441580e5f9b863b9a69a4938169b0cc

--- releasetools.py
+++ releasetools.py
@@ -19,14 +19,6 @@
 import common
 import re
 
-def FullOTA_Assertions(info):
-  AddBasebandAssertion(info, info.input_zip)
-  return
-
-def IncrementalOTA_Assertions(info):
-  AddBasebandAssertion(info, info.input_zip)
-  return
-
 def FullOTA_InstallEnd(info):
   OTA_InstallEnd(info)
   return
@@ -50,15 +42,4 @@ def OTA_InstallEnd(info):
   AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
   AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
   AddImage(info, "vbmeta_system.img", "/dev/block/bootdevice/by-name/vbmeta_system")
-  return
-
-def AddBasebandAssertion(info, input_zip):
-  android_info = input_zip.open("OTA/android-info.txt")
-  for line in android_info.readlines():
-    m = re.search(r'require\s+version-baseband\s*=\s*(.+)', line.decode('utf-8'))
-    if m:
-      hwc, modem_version, firmware_version = m.group(1).rstrip().split(',')
-      if (len(hwc) and len(modem_version) and len(firmware_version)):
-        cmd = 'assert(getprop("ro.boot.hwc") == "{0}" && (xiaomi.verify_baseband("{1}") == "1" || abort("ERROR: This package requires firmware from MIUI {2} or newer. Please upgrade firmware and retry!");) || true);'
-        info.script.AppendExtra(cmd.format(hwc, modem_version, firmware_version))
   return
