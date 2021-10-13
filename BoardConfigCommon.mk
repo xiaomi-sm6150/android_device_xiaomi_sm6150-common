6d2810da32ab14ecd380f05e1d5acd419675785b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-13 15:13:10 +0200

    sm6150-common: sepolicy: Bringup property_contexts for Android 13
    Change-Id: I60070ba7658ce262be66ee3bdd0cb6cdcf9b8c49

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -139,6 +139,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
 TARGET_SEPOLICY_DIR := msmsteppe
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Trust
