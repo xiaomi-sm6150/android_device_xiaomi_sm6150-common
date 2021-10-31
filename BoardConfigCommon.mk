42fdc25849f97c2733ddda6d60d4c0d44bd1e9bc
raghavt20 <raghavtandon2000@gmail.com>
2021-10-31 12:19:21 +0530

    [DNM]sm6150-common: Disable QCOM SEPolicy

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -140,7 +140,7 @@ VENDOR_SECURITY_PATCH := 2021-09-01
 
 # Sepolicy
 #TARGET_SEPOLICY_DIR := msmsteppe
-#include device/qcom/sepolicy_vndr/SEPolicy.mk
+TARGET_EXCLUDE_QCOM_SEPOLICY := true
 
 #SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 #BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
