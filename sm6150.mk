552cdd17f166644179b238c5d40fb29030180a62
Robert Shih <robertshih@google.com>
2022-02-14 16:02:55 -0800

    sm6150-common: Enable AIDL DRM HALs
    Bug: 219538389
    Test: atest GtsMediaTestCases
    Change-Id: Iaf321aabf7f0ef3d86c518982781d95c4a648506

--- sm6150.mk
+++ sm6150.mk
@@ -135,7 +135,7 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm-service.clearkey \
     android.hardware.drm@1.4.vendor
 
 # Fingerprint
