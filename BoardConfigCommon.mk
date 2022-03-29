3150c6fef24c74da6d6ab628293ce79295df1dc0
LuK1337 <priv.luk@gmail.com>
2022-03-30 00:30:07 +0200

    sm6150-common: Include vendor/debugfs.config
    This disables the following kernel configs:
    * CONFIG_DEBUG_FS
    * CONFIG_PAGE_OWNER
    
    Change-Id: Idbf74e3dee4dccff4708462271e79374e28449ae

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -79,7 +79,7 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6150
-TARGET_KERNEL_CONFIG := vendor/sdmsteppe-perf_defconfig
+TARGET_KERNEL_CONFIG := vendor/sdmsteppe-perf_defconfig vendor/debugfs.config
 
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x880000
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0
