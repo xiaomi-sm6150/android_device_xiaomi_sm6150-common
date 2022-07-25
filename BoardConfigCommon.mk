51211973b395a103772f22a46633a5eaba2b1ca3
helliscloser <mrumais@gmail.com>
2022-07-25 11:07:26 +0530

    sm6150-common: added Public and Private sepolicy

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -164,7 +164,8 @@ TARGET_SEPOLICY_DIR := msmsteppe
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
-SELINUX_IGNORE_NEVERALLOWS := true
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
