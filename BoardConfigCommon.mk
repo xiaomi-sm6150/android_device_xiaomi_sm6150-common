816cf6a1cd9eba0356fbbb02b8d36b238839e55c
Arian <arian.kulmer@web.de>
2022-08-18 16:05:15 +0200

    sm6150-common: Use LLVM binutils and LLVM integrated assembler
    Change-Id: I408c0e1b272af25e8e606e586a9e1d6e7082afba

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -86,6 +86,9 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6150
+TARGET_KERNEL_ADDITIONAL_FLAGS := \
+    LLVM=1 \
+    LLVM_IAS=1
 
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x880000
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0
