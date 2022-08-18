a2af0b073c93e1d3d0af409d16f353b3defa42fd
Arian <arian.kulmer@web.de>
2022-08-18 13:51:59 +0200

    sm6150-common: Drop audio.a2dp.default
    Change-Id: I7b72116797a2690f6b3c4f13b4edacf57fe937e5

--- sm6150.mk
+++ sm6150.mk
@@ -45,7 +45,6 @@ PRODUCT_PACKAGES += \
     android.hardware.soundtrigger@2.2-impl
 
 PRODUCT_PACKAGES += \
-    audio.a2dp.default \
     audio.bluetooth.default \
     audio.usb.default \
     audio.r_submix.default \
