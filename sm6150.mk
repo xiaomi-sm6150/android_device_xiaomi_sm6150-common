0e2890fafe022399ffbc971169448b2d13b7662b
Tim Zimmermann <tim@linux4.de>
2022-08-24 15:40:22 +0200

    sm6150-common: Inherit non_ab_device.mk
    * This is required to build applypatch for updating recovery
    
    Change-Id: I38053691f7724b5d6c30159c410d5d04ac136180

--- sm6150.mk
+++ sm6150.mk
@@ -14,6 +14,8 @@
 # limitations under the License.
 #
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
