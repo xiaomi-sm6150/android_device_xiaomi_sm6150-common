e194684060f951d5624100017b0ecf5dfcefe365
Arian <arian.kulmer@web.de>
2022-09-08 15:34:58 +0200

    sm6150-common: Move to legacy-um qcom sepolicy
    Change-Id: I9fa72d7c2313c663490c506780ff4c66ad8037a5

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -142,7 +142,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
 
 # Sepolicy
 TARGET_SEPOLICY_DIR := msmsteppe
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
