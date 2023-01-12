ec6b1f72bbb0dbf4c778922c9132df4def9b8772
basamaryan <basam.aryan@gmail.com>
2023-01-12 00:06:06 +0000

    sm6150-common: Build DRM 1.4 to vendor
    Change-Id: I1b642a1b0bc032f1e7f174bd072a485a20533fba

--- sm6150.mk
+++ sm6150.mk
@@ -135,7 +135,8 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm@1.4.vendor
 
 # Fingerprint
 PRODUCT_PACKAGES += \
