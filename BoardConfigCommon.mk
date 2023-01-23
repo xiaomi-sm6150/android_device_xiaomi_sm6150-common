ca7d8350166f2bd772b9bcff98a702a6e0afb553
Arian <arian.kulmer@web.de>
2023-01-24 00:31:02 +0100

    sm6150-common: Include qcom and lineage framework compatibility matrix
    Change-Id: I12ec7ed7ca6c1b6111f46b2441a2768b23ea85bc

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -66,6 +66,10 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 LOC_HIDL_VERSION := 4.0
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
+
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/configs/hidl/manifest.xml
 DEVICE_MANIFEST_FILE += hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/configs/hidl/compatibility_matrix.xml
