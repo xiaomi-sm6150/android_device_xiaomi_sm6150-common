3719d185b010676654b9ffaf2432934195a7d428
Arian <arian.kulmer@web.de>
2023-01-24 00:33:57 +0100

    sm6150-common: hidl: Add xiaomi framework compatibility matrix
    Change-Id: I6c8e4ecfc6391ea1d59c68ddb43bdca9f6180826

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -67,6 +67,7 @@ LOC_HIDL_VERSION := 4.0
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(COMMON_PATH)/configs/hidl/xiaomi_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 
