be567c830138ff4916f70a60d87aa98267331dc7
Nolen Johnson <johnsonnolen@gmail.com>
2023-01-25 20:55:33 -0500

    sm6150-common: Drop vestigial FM board-flags
    * No longer read anywhere.
    
    Change-Id: Iaf5d8cda03e055c4331e3a20e33c8058037b09b2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -58,9 +58,6 @@ TARGET_USES_HWC2 := true
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/configs/config.fs
 
-# FM
-BOARD_HAVE_QCOM_FM := true
-
 # GPS
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 LOC_HIDL_VERSION := 4.0
