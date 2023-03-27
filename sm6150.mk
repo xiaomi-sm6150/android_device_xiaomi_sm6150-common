702106ba2089b2a474ca5eeec93f1614e0617f3a
Gabriel Biren <gbiren@google.com>
2023-03-27 22:33:41 +0000

    sm6150-common: Update makefile to use the AIDL Wifi Vendor HAL.
    Bug: 274964641
    Test: Pre-submit tests
    Change-Id: I4d9114377fe75b593bbfe6a59f03d8f5d607d6a1

--- sm6150.mk
+++ sm6150.mk
@@ -412,7 +412,7 @@ PRODUCT_COPY_FILES += \
 
 # WiFi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     hostapd \
     ipacm \
     IPACM_cfg.xml \
