604f1d64a2a0dc15fa41b10f63c21c9d5e5fceae
raghavt20 <raghavt20@aospa.co>
2023-05-08 03:43:52 +0530

    sm6150-common: Use common adreno component
    Change-Id: I4f9a9b6591df27106c6858f0b27719a005043a83
    Signed-off-by: raghavt20 <raghavt20@aospa.co>

--- sm6150.mk
+++ sm6150.mk
@@ -111,9 +111,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     disable_configstore
 
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \
@@ -249,6 +246,7 @@ PRODUCT_COPY_FILES += \
 
 # QTI components
 TARGET_COMMON_QTI_COMPONENTS := \
+    adreno \
     bt \
     perf \
     telephony \
@@ -298,12 +296,6 @@ PRODUCT_COPY_FILES += \
 # Vulkan
 PRODUCT_PACKAGES += \
     libvulkan
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute.xml \
-    frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml \
-    frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml \
-    frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml
 
 # WiFi
 PRODUCT_PACKAGES += \
