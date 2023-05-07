25f7c76459becd3341d9832d5a6f4b3df612acb1
raghavt20 <raghavt20@aospa.co>
2023-05-07 18:32:41 +0530

    sm6150-common: Remove already included compatibility matrix
    Change-Id: I3491ec3a552e48cf6b37c0e258cb81988a336e64
    Signed-off-by: raghavt20 <raghavt20@aospa.co>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -71,7 +71,6 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
 
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/configs/hidl/manifest.xml
 DEVICE_MANIFEST_FILE += hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_manifest.xml
-DEVICE_MATRIX_FILE := $(COMMON_PATH)/configs/hidl/compatibility_matrix.xml
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
