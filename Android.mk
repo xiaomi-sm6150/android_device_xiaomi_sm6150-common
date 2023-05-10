41f02e5770054799ac6d692cbabd568a0df817df
ad1tyas1ngh <adityasingh1768@gmail.com>
2023-05-10 18:53:47 +0530

    sm6150-common: Rename davinci to davinciin
    Rebranded in order to avoid conflict with asus davinci
    
    Change-Id: I39b8e58393ff8cacf04628a482b49b768079e394
    Signed-off-by: ad1tyas1ngh <adityasingh1768@gmail.com>

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter courbet davinci phoenix surya sweet toco violet,$(TARGET_DEVICE)),)
+ifneq ($(filter courbet davinciin phoenix surya sweet toco violet,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
