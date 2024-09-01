3df5cd506cee0f4f91c957b37f58bba72f34fd39
basamaryan <basam.aryan@gmail.com>
2024-09-01 23:43:33 +0200

    sm6150-common: Commonize sepolicy
    Taken from davinci, courbet, surya, sweet, and sweet2
    
    Change-Id: Ie9bd3354d42a36e88004ee77343e0da5397a0eba

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -150,6 +150,7 @@ include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Soong
