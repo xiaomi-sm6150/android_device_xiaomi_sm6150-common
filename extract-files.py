ef390fea2f16cac777b4eb54af3f81ca07b95ac6
Michael Bestas <mkbestas@gmail.com>
2024-09-11 00:04:13 +0300

    sm6150-common: Shim widevine with libcrypto_shim
    The following change
    https://github.com/google/boringssl/commit/e202e51cb0912f36dafbd2e67cf04d6ec82f3180
    removed symbols that our libs require.
    
    Change-Id: I1325c34ecec3d339dd68c665010ecb36fa2e6a29

--- extract-files.py
+++ extract-files.py
@@ -4,6 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
 from extract_utils.fixups_lib import (
     lib_fixup_remove,
     lib_fixup_remove_arch_suffix,
@@ -46,9 +50,16 @@ lib_fixups: lib_fixups_user_type = {
     ): lib_fixup_remove,
 }
 
+
+blob_fixups: blob_fixups_user_type = {
+    ('vendor/lib64/mediadrm/libwvdrmengine.so', 'vendor/lib64/libwvhidl.so'): blob_fixup()
+        .add_needed('libcrypto_shim.so'),
+}  # fmt: skip
+
 module = ExtractUtilsModule(
     'sm6150-common',
     'xiaomi',
+    blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
     check_elf=True,
